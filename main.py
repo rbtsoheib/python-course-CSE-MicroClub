5f996d9550370eb756c3e6d1e43cca0388393fca
rbtsoheib <taharsoheib08@gmail.com>
2026-01-08 16:38:01 +0100

    more text

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ if 10000 < 27895678 :
 if 23456789 > 2345 :
 
     print('especially this one')
+
+
+if 234567890 < 12345678909876543231 :
+
+    print('hello man , i just thought of what could happen when you start USING YOUR FUCKING BRAIN DAMN IT')
+    print('FOR THE LAST MOTHERFUCKING TIME , SAY IT LOUDERRR')
+    print('I AM UPSET')
+
+
