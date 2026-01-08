873f18f6864284aacc4ae5d323141d39123aa3be
rbtsoheib <taharsoheib08@gmail.com>
2026-01-08 16:28:08 +0100

    learned how to use if statement in python

---
+++ main.py
@@ -0,0 +1,17 @@
+print('hello , world ')
+print('i am an absolute mothertfuvker')
+
+
+if 5 > 2 :
+
+    print('your mom is clastrophobic')
+
+
+if 10000 < 27895678 :
+
+    print('it is obvious motherblazer')
+
+
+if 23456789 > 2345 :
+
+    print('especially this one')
