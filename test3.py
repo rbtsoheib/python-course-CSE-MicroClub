d48c96851920ef13f3145d4ab04a0813c2414d78
rbtsoheib <taharsoheib08@gmail.com>
2026-02-21 22:19:16 +0100

    test 3 python

---
+++ test3.py
@@ -0,0 +1,9 @@
+g = 4 
+
+Eren = "Waited for this amount of time to make contact with his brother"
+
+Gabi = " Gabi sneaked SHOT Eren in his nape . "
+
+Zeke = " the time was so crucial that the moment she hit him, she seperated his head from his body, but before his conscious went off, he made contact with Zeke and transformed into his titan form"
+
+print(Eren + g + Gabi + Zeke)
