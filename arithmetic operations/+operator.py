10586cd8c74941d62742f7eeb211f6a4ba2c6a39
rbtsoheib <taharsoheib08@gmail.com>
2026-02-22 20:45:49 +0100

    + operator

---
+++ arithmetic operations/+operator.py
@@ -0,0 +1,12 @@
+a = 77 
+
+b = -8
+
+e = 3 
+f = 7
+
+d = e + f
+
+c = a + b 
+
+print(f"if you put it this way I never wanted to do the {c} position, but if you ensist I can make an exeption for the {d} in front of me")
