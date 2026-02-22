80bcdeb704acef4b8c7d068236393776083fa9b8
rbtsoheib <taharsoheib08@gmail.com>
2026-02-22 22:28:40 +0100

    dictionarries  experiments

---
+++ Lists Tuples Dictionnaries/dictionnaries.py
@@ -0,0 +1,22 @@
+
+firstdictionnary = {
+    "name" : "Jaefer",  
+    "age" : 20,
+    "city" : "Bab el Oued",
+    "studies" : "web developement" ,
+    "hobbies" : ["football", "gaming", "coding"],
+    "is_student" : True
+
+}
+
+print(firstdictionnary) 
+
+print(firstdictionnary["name"])
+print(firstdictionnary["age"])
+print(firstdictionnary["studies"])
+
+print(firstdictionnary.keys())
+print(firstdictionnary.values())
+print(firstdictionnary.items())
+
+
