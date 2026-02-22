9f55c5241661b3fd93fb08e5f14ea130910883d0
rbtsoheib <taharsoheib08@gmail.com>
2026-02-22 20:50:11 +0100

    - operator

---
+++ arithmetic operations/-operator.py
@@ -0,0 +1,11 @@
+z = 9987
+
+y = - 4567
+
+x = - (y - z)
+
+print(x)
+
+print(f"if only I had the choice, I would hav sold the land for {x}$")
+
+print("if you do not get it , it is a There will Blood reference hehe")
