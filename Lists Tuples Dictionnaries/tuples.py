9b4b579c670d19ea8d8db5aeda31a4974ad96260
rbtsoheib <taharsoheib08@gmail.com>
2026-02-22 22:11:53 +0100

    the first tuple

---
+++ Lists Tuples Dictionnaries/tuples.py
@@ -0,0 +1,12 @@
+tuple = (3 , 6 , " hello world !!!" , 32)
+
+print(tuple)
+
+print(tuple[0])
+print(tuple[1])
+print(tuple[2])
+print(tuple[3])
+
+
+
+print(" this is the first hello world sooo" + tuple[2])
