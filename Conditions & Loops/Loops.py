ce3be3dffc96621b4c0835e4aed9491782218d26
rbtsoheib <taharsoheib08@gmail.com>
2026-02-24 21:58:42 +0100

    all the code for the for loop , now the while

--- Conditions & Loops/Loops.py
+++ Conditions & Loops/Loops.py
@@ -1,47 +1,46 @@
 
 
-# fruits = ['apple', 'banana', 'cherry']
-# for fruit in fruits:
-#     print(fruit)   
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)   
 
 
-# for i in range(5):
-#     print(i)
+for i in range(5):
+    print(i)
 
 
-# for i in range(100, 0, -10): 
-#     print(f"the niggas are now {i}")
+for i in range(100, 0, -10): 
+    print(f"the niggas are now {i}")
 
 
-# for i in range(1, 10): 
-#     print(f"the niggas are now {i}")
-
+for i in range(1, 10): 
+    print(f"the niggas are now {i}")
 
 
 for i in range(1, 20): 
     print(f"the niggas are now {i}")
 
-# for i in range(2, 10, 2):
-#     print(i)
+for i in range(2, 10, 2):
+    print(i)
 
 
 
-# names = ['Soheib el chikor' , 'Wassim the autistic' , 'Yahia the goat']
-# for name in names :
-#     if name == 'Soheib el chikor' :
-#         print(f"{name} is the greatest men of all time (idk why I said that, but still cool)")
-#     elif name == 'Yahia the goat' :
-#         print(f"{name} is the goat of the groupe")
-#     else :
-#         print(f"{name} is the most autistic and stupid and blackest one in the groupe")
+names = ['Soheib el chikor' , 'Wassim the autistic' , 'Yahia the goat']
+for name in names :
+    if name == 'Soheib el chikor' :
+        print(f"{name} is the greatest men of all time (idk why I said that, but still cool)")
+    elif name == 'Yahia the goat' :
+        print(f"{name} is the goat of the groupe")
+    else :
+        print(f"{name} is the most autistic and stupid and blackest one in the groupe")
 
 
 
-# numbers = [1 , 3 , 6 , 8 , 0]
+numbers = [1 , 3 , 6 , 8 , 0]
 
-# for number in numbers :
-#     print(f"the number is {number}")
+for number in numbers :
+    print(f"the number is {number}")
 
-# for i in range(len(numbers)) :
-#     print(f"the number is {numbers[i]}")
+for i in range(len(numbers)) :
+    print(f"the number is {numbers[i]}")
 
