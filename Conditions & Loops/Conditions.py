7964bf92ae6cfb9d078682c068786298db07d9c7
rbtsoheib <taharsoheib08@gmail.com>
2026-02-24 21:32:18 +0100

    tried elif

--- Conditions & Loops/Conditions.py
+++ Conditions & Loops/Conditions.py
@@ -13,3 +13,20 @@ if (n < s) :
 
 else : 
     print(f"HAHAAAAA MY NAME IS {name} AND I AM A {Illegal} from {ethnecity}")
+
+
+
+a = 15 
+
+b = 45 
+
+c = 30
+
+if (a > b) :
+    print("a is greater than b") 
+elif (b < c) : 
+    print("b is less than c") 
+elif (a == c) :
+    print("a is equal to c")
+else :
+    print("b is the biggest and then c after that is a")
