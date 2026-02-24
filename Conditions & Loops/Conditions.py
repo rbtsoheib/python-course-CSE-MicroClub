18847d7db3edc013f336d790be7e0cd81d385d3d
rbtsoheib <taharsoheib08@gmail.com>
2026-02-24 20:57:53 +0100

    learning conditions

---
+++ Conditions & Loops/Conditions.py
@@ -0,0 +1,15 @@
+n = 9
+
+s = 4 
+
+name = "AL modamir 57"
+
+ethnecity = "Middle Eastern"
+
+Illegal = "minor"
+
+if (n < s) :
+    print("hello name is soheib") 
+
+else : 
+    print(f"HAHAAAAA MY NAME IS {name} AND I AM A {Illegal} from {ethnecity}")
